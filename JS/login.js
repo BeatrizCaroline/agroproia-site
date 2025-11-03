915f629e3594c31c8bc8bad3b7ccf44fe77ea818
Beatriz Caroline <149972851+BeatrizCaroline@users.noreply.github.com>
2025-11-03 17:52:22 -0300

    Update login.js Vs.1.1

--- JS/login.js
+++ JS/login.js
@@ -1,12 +1,31 @@
 
+// Botões de login/cadastro
 var btnSignin = document.querySelector("#signin");
 var btnSignup = document.querySelector("#signup");
 var body = document.querySelector("body");
 
-btnSignin.addEventListener("click", function () {
-  body.className = "sign-in-js"
+if (btnSignin) {
+  btnSignin.addEventListener("click", function () {
+    body.className = "sign-in-js";
+  });
+}
+
+if (btnSignup) {
+  btnSignup.addEventListener("click", function () {
+    body.className = "sign-up-js";
+  });
+}
+
+// Quando clicar no "X", vai para a tela de Home Deslogada
+document.addEventListener("DOMContentLoaded", () => {
+  const botoesFechar = document.querySelectorAll('#btnFechar');
+
+  botoesFechar.forEach(botao => {
+    botao.addEventListener("click", (e) => {
+      e.preventDefault();
+      window.location.href = "index.html";
+    });
+  });
 });
 
-btnSignup.addEventListener("click", function () {
-  body.className = "sign-up-js"
-});
+
